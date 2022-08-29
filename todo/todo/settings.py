fa9dbd956a4ec7fde658c640861f8f35f6851bc0
squadran2003 <cormackandy@hotmail.com>
2022-08-29 11:34:49 +0100

    fixed secret key issue

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bnz)&n)*7-or66zvh-$g!gt!02ozah0w%@b85o=&za!&_x2s-j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
